2c2c94e8ab175e9ba18746493686d914cb9f17cb
C B Sajan <sajan@ckhathri.com>
2022-05-03 13:43:09 +0530

    Deleted and Added Some File

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>Hello {{name}} !</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data(){
+            return{
+                name:'Sajan'
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+div{
+    color: red
 }
 </style>
