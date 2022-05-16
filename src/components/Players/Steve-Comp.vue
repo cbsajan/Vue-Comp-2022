4192f5e702319e005f0445ace0a35325dec425eb
C B Sajan <sajan@ckhathri.com>
2022-05-16 21:08:23 +0530

    17-Teleport

--- src/components/Players/Steve-Comp.vue
+++ src/components/Players/Steve-Comp.vue
@@ -1,17 +1,19 @@
 <template>
     <h3>Mike is on</h3>
+    <teleport to="#dialog_here">
+        <comp-modal></comp-modal>
+    </teleport>
 </template>
 
 <script>
+import compModal from "../Utils/Modal-Comp.vue";
 export default {
+    components: {
+        compModal
+    },
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Steve',
-    activated() {
-        console.log('hello Steve!!')
-    },
-    deactivated() {
-        console.log('by Steve!!')
-    },
+
     unmounted() {
         console.log('Steve unmounted')
     }
---
+++ src/components/Utils/Modal-Comp.vue
@@ -0,0 +1,15 @@
+<template>
+    <dialog open>
+        <h3>You are in the Modal</h3>
+    </dialog>
+</template>
+<style scoped>
+dialog {
+    position: fixed;
+    top: 20vh;
+    left: 0%;
+    width: 20%;
+    background-color: #b9b9b9;
+    text-align: center;
+}
+</style>
